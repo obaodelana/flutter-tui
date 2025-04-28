f959c06bfbea263235edb28167b549076b904df0
obaodelana <obaloluwaode@gmail.com>
2025-04-28 09:46:27 -0600

    Attempting to implement stateful widgets

--- lib/src/framework.dart
+++ lib/src/framework.dart
@@ -3,26 +3,38 @@ import 'package:meta/meta.dart';
 import 'package:flutter_tui/src/models/size.dart';
 import 'package:flutter_tui/src/models/write_object.dart';
 
-abstract class Widget {
-  const Widget();
-
+abstract class Writable {
   WriteObject build(Size constraint);
+}
+
+abstract class Widget implements Writable {
+  const Widget();
 }
 
 abstract class StatelessWidget extends Widget {
   const StatelessWidget();
 }
 
-// abstract class StatefulWidget extends Widget {
-//   const StatefulWidget();
+abstract class StatefulWidget extends Widget {
+  late final State _state;
 
-//   @protected
-//   State createState();
-// }
+  StatefulWidget() {
+    _state = createState();
+    _state.widget = this;
+  }
 
+  @protected
+  State createState();
 
-// abstract class State<T extends StatefulWidget> {
-//   T? _widget;
+  @override
+  WriteObject build(Size constraint) {
+    return _state.build(constraint);
+  }
+}
 
-//   WriteObject build(Size constraint);
-// }
+abstract class State<T extends StatefulWidget> implements Writable {
+  T? _widget;
+
+  T? get widget => _widget;
+  set widget(T? widget) => _widget ??= widget;
+}

9edea0026294a2cbea799fc8bc12376e3eb3cab3
obaodelana <obaloluwaode@gmail.com>
2025-04-29 12:13:41 -0600

    More robust example added

--- bin/flutter_tui.dart
+++ bin/flutter_tui.dart
@@ -1,15 +1,35 @@
 import 'package:flutter_tui/flutter_tui.dart';
 import 'package:flutter_tui/src/widgets.dart';
 
-void main(List<String> arguments) {
-  runApp(Align(
-    alignment: Alignment.centerRight,
+void main(List<String> arguments) async {
+  await runApp(Center(
     child: Column(
+      mainAxisAlignment: AxisAlignment.center,
       children: [
-        for (int i = 0; i < 7; i++)
-          Text("${i+1}")
+        Row(
+          mainAxisAlignment: AxisAlignment.center,
+          children: [
+            for (int i = 0; i < 3; i++)
+              Text("Text ${i+1}")
+          ]
+        ),
+        Row(
+          mainAxisAlignment: AxisAlignment.center,
+          crossAxisAlignment: AxisAlignment.center,
+          children: [
+            for (int i = 3; i < 3+3; i++)
+              Text("Text ${i+1}")
+          ]
+        ),
+        Row(
+          mainAxisAlignment: AxisAlignment.center,
+          crossAxisAlignment: AxisAlignment.end,
+          children: [
+            for (int i = 3+3; i < 3+3+3; i++)
+              Text("Text ${i+1}")
+          ]
+        ),
       ]
     )
-  )
-  );
+  ));
 }
